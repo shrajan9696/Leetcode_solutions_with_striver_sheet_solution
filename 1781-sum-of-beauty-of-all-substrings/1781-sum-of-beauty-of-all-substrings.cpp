7aebe452ba47547baa9330f0e2f38d6242d009d5
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2022-11-16 13:22:52 +0530

    Time: 687 ms (54.77%), Space: 26.6 MB (13.96%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int beautySum(string s) {
-       
         int ans=0;
-       
+        
         for(int i=0;i<s.length();i++){
-          
-        int mx = 0;
-          
-            map<char,int> m;
-            for(int j=i;j<s.length();j++)
-            {
-                   int mn = INT_MAX;
+            int mx =1;
+            map<int,int> m;
+            int arr[26] ={0};
+            for(int j=i;j<s.length();j++){
+                
+                int mn = INT_MAX;
                 m[s[j]]++;
+                arr[s[j]-'a']++;
                 mx = max(mx,m[s[j]]);
                
-              
-               
-               for(auto i : m) mn = min(mn, i.second);
-           
-                // cout<<mx<<" "<<mn<<"\n";
-    
+               for(int k=0;k<26;k++ ){
+                   if(arr[k]!=0){
+                   mn = min(mn,arr[k]);
+                   }
+               }
+                // for(auto x: m){
+                //     mn = min(mn,x.second);
+                // }
+                //  cout<<mx<<" "<<mn<<" ";
                 ans+=(mx-mn);
-                // cout<<ans<<" ";
             }
         }
         return ans;
