3790bed2ef10a192970f2cf458b8376a2ab6ba0b
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2022-12-01 20:54:40 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution {
 public:
 
-    void DFS(vector<vector<int>>&image,int row,int column, int color,vector<vector<bool>>&visited,int scol)
+    void DFS(vector<vector<int>>&image,int row,int column, int color,vector<vector<bool>>&visited,int scol,int drow[],int dcol[])
     {
       
         visited[row][column] = true;
@@ -15,26 +15,34 @@ public:
           int m = image[0].size();
             // int x = image[row][column];
             image[row][column] = color;
-            if(row-1>=0 and image[row-1][column] == scol and visited[row-1][column] == false)
-            {
-                visited[row-1][column] = true;
-                DFS(image,row-1,column,color,visited,scol);
+            // if(row-1>=0 and image[row-1][column] == scol and visited[row-1][column] == false)
+            // {
+            //     visited[row-1][column] = true;
+            //     DFS(image,row-1,column,color,visited,scol);
+            // }
+            // if(row+1<n and image[row+1][column] == scol and visited[row+1][column] == false)
+            // {
+            //     visited[row+1][column] = true;
+            //       DFS(image,row+1,column,color,visited,scol);
+            // }
+            // if(column-1>=0 and image[row][column-1] == scol and visited[row][column-1] == false)
+            // {
+            //     visited[row][column-1] = true;
+            //     DFS(image,row,column-1,color,visited,scol);
+            // }
+            // if(column+1<m and image[row][column+1] == scol and visited[row][column+1] == false)
+            // {
+            //     visited[row][column+1] = true;
+            //      DFS(image,row,column+1,color,visited,scol);
+            // }
+            for(int i=0;i<4;i++){
+                int nrow =row+drow[i];
+                int ncol = column +dcol[i];
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and visited[nrow][ncol]==false and image[nrow][ncol]==scol){
+                      DFS(image,nrow,ncol,color,visited,scol,drow,dcol);
+                }
             }
-            if(row+1<n and image[row+1][column] == scol and visited[row+1][column] == false)
-            {
-                visited[row+1][column] = true;
-                  DFS(image,row+1,column,color,visited,scol);
-            }
-            if(column-1>=0 and image[row][column-1] == scol and visited[row][column-1] == false)
-            {
-                visited[row][column-1] = true;
-                DFS(image,row,column-1,color,visited,scol);
-            }
-            if(column+1<m and image[row][column+1] == scol and visited[row][column+1] == false)
-            {
-                visited[row][column+1] = true;
-                 DFS(image,row,column+1,color,visited,scol);
-            }
+            
         
         
     }
@@ -43,6 +51,8 @@ public:
         int n = image.size();
         int m = image[0].size();
         vector<vector<bool>> visited(n,vector<bool>(m, false));
+        int drow[4] = {-1,0,1,0};
+        int dcol[4] = {0,1,0,-1};
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++){
@@ -50,7 +60,7 @@ public:
             }
         }
         int scol = image[sr][sc];
-        DFS(image,sr,sc,newColor,visited,scol);
+        DFS(image,sr,sc,newColor,visited,scol,drow,dcol);
         return image;
     }
 };
