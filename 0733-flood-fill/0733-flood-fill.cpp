08df6a21a543dba86788c8f5a0184dd1fd02480e
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2022-12-01 20:59:11 +0530

    Time: 19 ms (50.68%), Space: 14.2 MB (27.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
-    
-       void BFS(vector<vector<int>>&image,int r,int c, int color,vector<vector<bool>>&visited)
+      void DFS(vector<vector<int>>&image,int row,int column, int color,vector<vector<bool>>&visited,int scol,int drow[],int dcol[])
     {
+      
+        visited[row][column] = true;
+       
+          int n = image.size();
+          int m = image[0].size();
+          image[row][column] = color;
+          
+             for(int i=0;i<4;i++)
+             {
+                int nrow =row+drow[i];
+                int ncol = column +dcol[i];
+                if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and visited[nrow][ncol]==false and image[nrow][ncol]==scol)
+                {
+                      DFS(image,nrow,ncol,color,visited,scol,drow,dcol);
+                }
+             }
+    }
+            
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         int n = image.size();
         int m = image[0].size();
-        queue<pair<int,int>> q;
-        q.push({r,c});
-        visited[r][c] = false;
-        while(!q.empty()){
-            auto u = q.front();
-            q.pop();
-            int row = u.first;
-            int column = u.second;
-            int x = image[row][column];
-            image[row][column] = color;
-            if(row-1>=0 and image[row-1][column] == x and visited[row-1][column] == false){
-                visited[row-1][column] = true;
-                q.push({row-1,column});
-            }
-            if(row+1<n and image[row+1][column] == x and visited[row+1][column] == false){
-                visited[row+1][column] = true;
-                q.push({row+1,column});
-            }
-              if(column-1>=0 and image[row][column-1] == x and visited[row][column-1] == false){
-                visited[row][column-1] = true;
-                q.push({row,column-1});
-            }
-            if(column+1<m and image[row][column+1] == x and visited[row][column+1] == false){
-                visited[row][column+1] = true;
-                q.push({row,column+1});
-            }
-        }
-        
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-          int n = image.size();
-        int m = image[0].size();
         vector<vector<bool>> visited(n,vector<bool>(m, false));
+        int drow[4] = {-1,0,1,0};
+        int dcol[4] = {0,1,0,-1};
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++){
                 visited[i][j] = false;
             }
         }
-        BFS(image,sr,sc,newColor,visited);
+        int scol = image[sr][sc];
+        DFS(image,sr,sc,newColor,visited,scol,drow,dcol);
         return image;
     }
 };
