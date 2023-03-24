cfcf1d9c9474fb8662bc47a75f45e8e702cb2c95
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2023-03-24 23:27:18 +0530

    Time: 3 ms (99.75%), Space: 8.2 MB (49.11%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,33 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        map<ListNode*,bool> m;
-        ListNode*curr = head;
+        
+        // changes in structure of linked list
+        
+//         map<ListNode*,bool> m;
+//         ListNode*curr = head;
+//         while(curr!=NULL){
+//             if(m[curr] == true) return true;
+//             m[curr] = true;
+//             curr = curr->next;
+            
+//         }
+//         return false;
+        
+        // changes in the pointer of linked list
+        
+   ListNode*temp = new ListNode(0);
+        ListNode* curr = head;
         while(curr!=NULL){
-            if(m[curr] == true) return true;
-            m[curr] = true;
-            curr = curr->next;
+            if(curr->next == temp) return true;
+            ListNode*x = curr->next;
+            curr->next = temp;
+            curr = x;
             
         }
         return false;
+        
+        
+        
     }
 };
