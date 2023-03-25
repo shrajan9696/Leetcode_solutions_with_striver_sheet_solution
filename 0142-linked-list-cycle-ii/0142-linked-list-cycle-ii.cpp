19880f7931d94f427bd7563c09d9cd2db6fe9e76
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2023-03-25 12:11:50 +0530

    Time: 21 ms (8.39%), Space: 10 MB (9.28%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*curr=head;
+        set<ListNode*> s;
+        while(curr!=NULL and curr->next!=NULL){
+            if(s.find(curr)!=s.end()){
+                return curr;
+            }
+            s.insert(curr);
+            curr = curr->next;
+        }
+        return NULL;
+    }
+};
