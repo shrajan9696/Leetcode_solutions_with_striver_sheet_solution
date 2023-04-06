0582af65b0797a6ea01337f03bd4063e54a2605e
Shrajan jain <89202642+shrajan9696@users.noreply.github.com>
2023-04-06 16:10:37 +0530

    Time: 9 ms (89.99%), Space: 10.4 MB (78.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,0);
-        dp[0] = 1;
-        for(int i=1;i<n;i++){
+        // O(n*n) solution
+        
+//         int n = nums.size();
+//         vector<int> dp(n,0);
+//         dp[0] = 1;
+//         for(int i=1;i<n;i++){
             
          
        
-                dp[i] = 1;
-                for(int j=0;j<i;j++){
-                    if(nums[j]<nums[i]) dp[i] = max(dp[i],dp[j]+1);
-                }
+//                 dp[i] = 1;
+//                 for(int j=0;j<i;j++){
+//                     if(nums[j]<nums[i]) dp[i] = max(dp[i],dp[j]+1);
+//                 }
             
+//         }
+//         return *max_element(dp.begin(),dp.end());
+        
+        int n = nums.size();
+          vector<int> dp;
+        dp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i]>dp.back()) dp.push_back(nums[i]);
+           
+            else{
+                 auto it = lower_bound(dp.begin(),dp.end(),nums[i]);
+                   dp[it-dp.begin()] = nums[i];
+              
+            }
+        //      for(auto x:dp) cout<<x<<" ";
+        // cout<<"\n";
         }
-        return *max_element(dp.begin(),dp.end());
+       
+        return dp.size();
+        
+        
     }
 };
